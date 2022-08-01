305dde68265ef3ad707635c2475028c5d017a957
Geonwoo Lee <gwlee@kakao.com>
2022-08-01 13:57:30 +0900

    edit index

--- src/assets/sampleData.js
+++ src/assets/sampleData.js
@@ -11,12 +11,12 @@ const sampleData = [
     {
         title: "일정그룹 A~B",
         id: "0",
-        test: "테스트중",
-        status: "상태값A",
+        status: "로봇",
         schedule: [
             {
                 title: "일정 A",
                 id: "0B",
+                sc: "테스트",
                 start: `${today} 03:00:00`,
                 end: `${today} 05:00:00`,
                 data: {
@@ -28,6 +28,7 @@ const sampleData = [
             {
                 title: "일정 B",
                 id: "0A",
+                sc: "테스트",
                 start: `${today} 12:00:00`,
                 end: `${today} 15:00:00`,
             },
@@ -36,22 +37,26 @@ const sampleData = [
     {
         title: "일정그룹 C~E",
         id: "1",
+        status: "로봇",
         schedule: [
             {
                 title: "일정 C",
                 id: "1A",
+                sc: "테스트",
                 start: `${today} 00:00:00`,
                 end: `${today} 04:00:00`,
             },
             {
                 title: "일정 D",
                 id: "1Aaa",
+                sc: "테스트",
                 start: `${today} 05:00:00`,
                 end: `${today} 10:00:00`,
             },
             {
                 title: "일정 E",
                 id: "1B",
+                sc: "테스트",
                 start: `${today} 10:00:00`,
                 end: `${today} 15:00:00`,
             },
@@ -60,22 +65,26 @@ const sampleData = [
     {
         title: "일정그룹 F~H",
         id: "2",
+        status: "로봇",
         schedule: [
             {
                 title: "일정 F",
                 id: "2asdfA",
+                sc: "테스트",
                 start: `${today} 04:00`,
                 end: `${today} 10:00`,
             },
             {
                 title: "일정 G",
                 id: "2B",
+                sc: "테스트",
                 start: `${today} 10:30`,
                 end: `${today} 19:00`,
             },
             {
                 title: "일정 H",
                 id: "2asdA",
+                sc: "테스트",
                 start: `${yesterday} 22:00`,
                 end: `${today} 02:00`,
             },
@@ -84,10 +93,12 @@ const sampleData = [
     {
         title: "일정그룹 I~J",
         id: "3",
+        status: "로봇",
         schedule: [
             {
                 title: "일정 I",
                 id: "3A",
+                sc: "테스트",
                 start: `${today} 07:30`,
                 end: `${today} 14:00`,
             },
@@ -102,11 +113,12 @@ const sampleData = [
     {
         title: "일정그룹 K나혼자~",
         id: "4",
-        status: "상태값B",
+        status: "로봇",
         schedule: [
             {
                 title: "일정 K",
                 id: "4A",
+                sc: "테스트",
                 start: `${today} 22:30`,
                 end: `${today} 23:40`,
             },
@@ -119,11 +131,12 @@ const sampleData2 = [
         title: "일정그룹 A~B",
         id: "0",
         test: "테스트중",
-        status: "상태값A",
+        status: "로봇",
         schedule: [
             {
                 title: "일정 Aaabba",
                 id: "0Babbab",
+                sc: "테스트",
                 start: `${today} 03:00:00`,
                 end: `${today} 05:00:00`,
                 data: {
@@ -135,6 +148,7 @@ const sampleData2 = [
             {
                 title: "일정 B",
                 id: "0A",
+                sc: "테스트",
                 start: `${today} 12:00:00`,
                 end: `${today} 15:00:00`,
             },
@@ -143,22 +157,26 @@ const sampleData2 = [
     {
         title: "일정그룹 C~E",
         id: "1",
+        status: "로봇",
         schedule: [
             {
                 title: "일정 C",
                 id: "1A",
+                sc: "테스트",
                 start: `${today} 00:00:00`,
                 end: `${today} 09:00:00`,
             },
             {
                 title: "일정 D",
                 id: "1Aaa",
+                sc: "테스트",
                 start: `${today} 05:00:00`,
                 end: `${today} 10:00:00`,
             },
             {
                 title: "일정 E",
                 id: "1B",
+                sc: "테스트",
                 start: `${today} 10:00:00`,
                 end: `${today} 15:00:00`,
             },
@@ -167,22 +185,26 @@ const sampleData2 = [
     {
         title: "일정그룹 F~H",
         id: "2",
+        status: "로봇",
         schedule: [
             {
                 title: "일정 F",
                 id: "2asdfA",
+                sc: "테스트",
                 start: `${today} 04:00`,
                 end: `${today} 10:00`,
             },
             {
                 title: "일정 G",
                 id: "2B",
+                sc: "테스트",
                 start: `${today} 10:30`,
                 end: `${today} 19:00`,
             },
             {
                 title: "일정 H",
                 id: "2asdA",
+                sc: "테스트",
                 start: `${yesterday} 22:00`,
                 end: `${today} 02:00`,
             },
@@ -191,16 +213,19 @@ const sampleData2 = [
     {
         title: "일정그룹 I~J",
         id: "3",
+        status: "로봇",
         schedule: [
             {
                 title: "일정 I",
                 id: "3A",
+                sc: "테스트",
                 start: `${today} 07:30`,
                 end: `${today} 14:00`,
             },
             {
                 title: "일정 J",
                 id: "3B",
+                sc: "테스트",
                 start: `${today} 15:30`,
                 end: `${tomorrow} 20:00`,
             },
@@ -209,11 +234,12 @@ const sampleData2 = [
     {
         title: "일정그룹 K만",
         id: "4",
-        status: "상태값B",
+        status: "로봇",
         schedule: [
             {
                 title: "일정 K",
                 id: "4A",
+                sc: "테스트",
                 start: `${today} 22:30`,
                 end: `${today} 23:40`,
             },
